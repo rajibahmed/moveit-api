479fd4cb99abaa8feb0c61c8b219c601dfe6050a
Rajib Ahmed <rajib@magplus.com>
2016-11-22 15:21:14 +0100

    Remove comment on routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   resources :offers, only: [:show, :create]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-	get "/health-check" => "welcome#health"
 
+	get "/health-check" => "welcome#health"
 	get 'distance' => 'distance#index'
 end
